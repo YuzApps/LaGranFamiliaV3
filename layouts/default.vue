19d29ec9829a9891b52c6ef4d673d64e1863ace4
Yusniel Brocard <brocard@gmail.com>
2024-01-23 07:05:03 -0600

    wip

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+
+</script>
+
+<template>
+  <div>
+    Layout: default
+    <slot />
+    <SpeedInsights />
+  </div>
+</template>
+
+<style scoped></style>
