aa2da9552a945ce3b116d5f00a073058aa17ed1b
Yusniel Brocard <brocard@gmail.com>
2024-01-29 09:06:54 -0600

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/google-fonts"],
   devtools: { enabled: true },
 
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+  },
+
   googleFonts: {
     families: {
       Montserrat: [100, 200, 400, 500, 600, 700],
