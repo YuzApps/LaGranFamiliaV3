64ee2c3d62fe67346ad9da44d160368229c9044f
Yusniel Brocard <brocard@gmail.com>
2024-01-29 23:31:58 -0600

    wip

---
+++ components/hero/HeroSectionQuoteImage.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="relative isolate overflow-hidden pt-10">
+    <NuxtImg
+      src="/banner/anton-8q-U8X1zkvI-unsplash.jpg"
+      alt=""
+      class="absolute inset-0 -z-10 h-full w-full object-cover"
+    />
+    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+      <div
+        class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#006897] opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-32">
+      <div class="text-center">
+        <h1 class="text-2xl font-medium font-montserrat tracking-wide text-white sm:text-2xl">
+          En la clínica me encontré cuidados de alta calidad y un trato cálido. Me sentí como en casa y completamente
+          atendida. ¡Altamente recomendable!
+        </h1>
+        <p class="mt-6 text-lg leading-8 text-gray-300">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+          fugiat veniam occaecat fugiat aliqua.
+        </p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="#"
+            class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            >Get started</a
+          >
+          <a href="#" class="text-sm font-semibold leading-6 text-white"
+            >Learn more <span aria-hidden="true">→</span></a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="absolute inset-x-0 top-[calc(100%-13rem)] bg-bahama-blue/50 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      aria-hidden="true"
+    >
+      <div
+        class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#006897] opacity-40 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -14,5 +14,6 @@ useSeoMeta({
     <HeroSection />
     <HeroSectionQuote />
     <HeroSectionFeature />
+    <HeroSectionQuoteImage />
   </div>
 </template>
