f51bc42a1dc3951fdb55fc8fd2d709b69126f6b9
Yusniel Brocard <brocard@gmail.com>
2024-01-30 10:52:45 -0600

    updates: add page services

---
+++ components/hero/HeroSectionPageServices.vue
@@ -0,0 +1,160 @@
+<script setup lang="ts">
+const pricing = {
+  frequencies: [
+    { value: "monthly", label: "Monthly", priceSuffix: "/month" },
+    { value: "annually", label: "Annually", priceSuffix: "/year" },
+  ],
+  tiers: [
+    {
+      name: "Freelancer",
+      id: "tier-freelancer",
+      href: "#",
+      price: { monthly: "$30", annually: "$288" },
+      description: "The essentials to provide your best work for clients.",
+      features: ["5 products", "Up to 1,000 subscribers", "Basic analytics", "48-hour support response time"],
+      mostPopular: false,
+    },
+    {
+      name: "Startup",
+      id: "tier-startup",
+      href: "#",
+      price: { monthly: "$60", annually: "$576" },
+      description: "A plan that scales with your rapidly growing business.",
+      features: [
+        "25 products",
+        "Up to 10,000 subscribers",
+        "Advanced analytics",
+        "24-hour support response time",
+        "Marketing automations",
+      ],
+      mostPopular: true,
+    },
+    {
+      name: "Enterprise",
+      id: "tier-enterprise",
+      href: "#",
+      price: { monthly: "$90", annually: "$864" },
+      description: "Dedicated support and infrastructure for your company.",
+      features: [
+        "Unlimited products",
+        "Unlimited subscribers",
+        "Advanced analytics",
+        "1-hour, dedicated support response time",
+        "Marketing automations",
+        "Custom reporting tools",
+      ],
+      mostPopular: false,
+    },
+  ],
+};
+</script>
+<template>
+  <div>
+    <div class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
+      <NuxtImg src="/banner/hero-services.png" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
+      <div
+        class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
+      </div>
+      <div
+        class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
+      </div>
+      <div class="mx-auto max-w-4xl text-center">
+        <span class="uppercase text-[16px] font-maven text-gray-600">Servicios</span>
+        <h2 class="text-[47px] font-maven font-semibold tracking-tight text-white sm:text-6xl">¿Qué Ofrecemos?</h2>
+        <p class="mt-6 text-[18px] leading-8 text-gray-300 font-montserrat">
+          Brindamos consultas médicas, exámenes, evaluaciones y tratamientos. Siempre estamos listos para satisfacer sus
+          necesidades y mejorar su salud. Si busca soluciones para sus inquietudes médicas, contáctenos hoy para
+          programar una cita. Estamos abiertos los 7 días de la semana.
+        </p>
+      </div>
+    </div>
+
+    <!-- Pricing section -->
+    <div class="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 lg:px-8 py-10">
+      <div class="mx-auto max-w-4xl text-center">
+        <h1 class="text-base font-semibold leading-7 text-[#099541]/45">SERVICIOS</h1>
+        <p class="mt-2 text-4xl font-medium font-maven text-tropaz-blue tracking-tight sm:text-5xl">
+          Nuestros Servicios
+        </p>
+      </div>
+      <p class="mx-auto mt-6 max-w-5xl text-center font-montserrat text-lg leading-8 text-stratos-blue">
+        Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer
+        loyalty, and driving sales.
+      </p>
+
+      <div
+        class="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3"
+      >
+        <div
+          v-for="tier in pricing.tiers"
+          :key="tier.id"
+          :class="[tier.mostPopular ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-200', 'rounded-3xl p-8']"
+        >
+          <h2
+            :id="tier.id"
+            :class="[tier.mostPopular ? 'text-indigo-600' : 'text-gray-900', 'text-lg font-semibold leading-8']"
+          >
+            {{ tier.name }}
+          </h2>
+          <p class="mt-4 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
+          <ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-600">
+            <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
+              <UIcon name="i-heroicons-check" class="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+              {{ feature }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/services.vue
+++ pages/services.vue
@@ -1,8 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+useSeoMeta({
+  title: "Servicios",
+  ogTitle: "Servicios",
+  description: "Conoce los servicios que ofrecemos en la Clínica General La Gran Familia.",
+  ogDescription: "Conoce los servicios que ofrecemos en la Clínica General La Gran Familia.",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
+});
+</script>
 
 <template>
   <div>
-    Page: foo
+    <HeroSectionPageServices />
   </div>
 </template>
 
