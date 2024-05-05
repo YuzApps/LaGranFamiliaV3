092fc2e3a33bcdd510365bc7b074bc0d6021a1ee
Yusniel Brocard <brocard@gmail.com>
2024-05-04 19:30:39 -0600

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,16 @@
 // @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  {
+    rules: {
+      'import/no-named-as-default': 'off',
+      'no-restricted-exports': [
+        'error',
+        {
+          restrictedNamedExports: ['default'],
+        },
+      ],
+    },
+  }
+)
