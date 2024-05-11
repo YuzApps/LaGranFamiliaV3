91e7ee4b2ee79eeb644dbaeac33c03c6ff86e787
Yusniel Brocard <brocard@gmail.com>
2024-05-11 10:38:53 -0600

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,59 +2,6 @@
 import { isDevelopment, isWindows } from 'std-env';
 
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/fonts',
-    '@nuxt/eslint',
-    'nuxt-og-image',
-    '@nuxthq/studio',
-    ...(isDevelopment || isWindows ? [] : ['nuxt-security']),
-  ],
-
-  fonts: {
-    families: [
-      { name: 'Montserrat', provider: 'google' },
-      { name: 'Maven Pro', provider: 'google' },
-    ],
-    priority: ['google'],
-  },
-
-  experimental: {
-    sharedPrerenderData: true,
-    payloadExtraction: true,
-  },
-
-  tailwindcss: {
-    configPath: '~/config/tailwind',
-  },
-
-  image: {
-    domains: ['portal.clinicalagranfamilia.com', 'clinicalagranfamilia.com', 'res.cloudinary.com'],
-    cloudinary: {
-      baseURL: 'https://res.cloudinary.com/assetss/image/upload/',
-    },
-  },
-
-  routeRules: {
-    '/': { prerender: true },
-    '/contact': { prerender: true },
-  },
-
-  devtools: {
-    enabled: false,
-  },
-
-  colorMode: {
-    preference: 'light',
-    fallback: 'system',
-  },
-
-  ui: {
-    icons: ['mdi', 'lucide', 'medical-icon'], //'lucide', 'fontawesome', 'heroicons', 'mdi', 'carbon',
-  },
-
   app: {
     head: {
       htmlAttrs: {
@@ -69,6 +16,55 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
   },
 
+  build: {
+    transpile: ['@googlemaps/js-api-loader'],
+  },
+
+  colorMode: {
+    preference: 'light',
+    fallback: 'system',
+  },
+
+  devtools: {
+    enabled: false,
+  },
+
+  experimental: {
+    sharedPrerenderData: true,
+    payloadExtraction: true,
+  },
+
+  fonts: {
+    families: [
+      { name: 'Montserrat', provider: 'google' },
+      { name: 'Maven Pro', provider: 'google' },
+    ],
+    priority: ['google'],
+  },
+
+  image: {
+    domains: ['portal.clinicalagranfamilia.com', 'clinicalagranfamilia.com', 'res.cloudinary.com'],
+    cloudinary: {
+      baseURL: 'https://res.cloudinary.com/assetss/image/upload/',
+    },
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    'nuxt-og-image',
+    '@nuxthq/studio',
+    ...(isDevelopment || isWindows ? [] : ['nuxt-security']),
+  ],
+
+  routeRules: {
+    '/': { prerender: true },
+    '/contact': { prerender: true },
+  },
+
   runtimeConfig: {
     public: {
       googleMapsApiKey: '',
@@ -80,7 +76,64 @@ export default defineNuxtConfig({
     },
   },
 
-  build: {
-    transpile: ['@googlemaps/js-api-loader'],
+  //
+  security: {
+    headers: {
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        'default-src': ["'self'"],
+        'base-uri': ["'self'"],
+        'connect-src': ["'self'", 'https:', 'http:', 'wss:', 'ws:'],
+        'font-src': ["'self'", '*.gstatic.com', '*.googleapis.com'],
+        'form-action': ["'none'"],
+        'frame-ancestors': ["'none'"],
+        'frame-src': ['https:'],
+        'img-src': ["'self'", 'https:', 'http:', 'data:', 'blob:'],
+        'manifest-src': ["'self'"],
+        'media-src': ["'self'", 'https:', 'http:'],
+        'object-src': ["'none'"],
+        'script-src': ["'self'", "'unsafe-inline'", "'wasm-unsafe-eval'"],
+        'script-src-elem': [
+          "'self'",
+          "'unsafe-inline'",
+          "'wasm-unsafe-eval'",
+          '*.youtube.com',
+          '*.google.com',
+          '*.googleapis.com',
+        ],
+        'script-src-attr': ["'unsafe-inline'"],
+        'style-src': ["'self'", "'unsafe-inline'", '*.googleapis.com', '*.gstatic.com'],
+        'upgrade-insecure-requests': true,
+      },
+    },
+    csrf: {
+      cookie: {
+        secure: true,
+        sameSite: 'strict',
+      },
+    },
+    csp: {
+      reportOnly: true,
+      hashAlgorithm: 'sha256',
+      policies: {
+        'script-src': [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-hashes'",
+          '*.youtube.com',
+          '*.google.com',
+          '*.googleapis.com',
+        ],
+      },
+    },
+    rateLimiter: false,
+  },
+
+  tailwindcss: {
+    configPath: '~/config/tailwind',
+  },
+
+  ui: {
+    icons: ['mdi', 'lucide', 'medical-icon'], //'lucide', 'fontawesome', 'heroicons', 'mdi', 'carbon',
   },
 });
