a741a9f8e28989e3ca2ef150995da5b657de4493
Yusniel Brocard <brocard@gmail.com>
2024-06-23 10:46:06 -0600

    wip

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,4 @@
 export default defineAppConfig({
-  header: {
-    links: [
-      { name: 'Inicio', href: '/' },
-      { name: 'Servicios', href: '/services' },
-      { name: 'Galería', href: '/gallery' },
-      { name: 'Citas', href: '/appointment' },
-    ],
-  },
-
   nuxtIcon: {
     iconifyApiOptions: {
       publicApiFallback: true,
--- app/components/TheHeading.vue
+++ app/components/TheHeading.vue
@@ -1,5 +1,5 @@
 <script setup>
-  const { header } = useAppConfig();
+  const { data: header } = await useAsyncData('header', () => queryContent('/header').findOne());
   const mobileMenuOpen = ref(false);
 </script>
 
@@ -29,7 +29,7 @@
         <NuxtLink
           v-for="item in header.links"
           :key="item.name"
-          :to="item.href"
+          :to="item.url"
           class="router-link"
           >{{ item.name }}</NuxtLink
         >
@@ -48,23 +48,10 @@
           class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
         >
           <div class="flex items-center justify-between">
-            <button
-              type="button"
-              class="-m-2.5 rounded-md p-2.5 text-gray-700"
-              @click="mobileMenuOpen = false"
-            >
+            <UButton color="gray" variant="ghost" @click="mobileMenuOpen = false">
               <span class="sr-only">Close menu</span>
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
+              <IconCloseMenu />
+            </UButton>
           </div>
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
