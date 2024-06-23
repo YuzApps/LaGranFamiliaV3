32eae001461b1dda1c85b657e293c9dc6440ddb1
Yusniel Brocard <brocard@gmail.com>
2024-06-23 09:30:28 -0600

    wip

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,16 @@
 export default defineAppConfig({
   header: {
     links: [
-      { name: "Inicio", href: "/" },
-      { name: "Servicios", href: "/services" },
-      { name: "Galería", href: "/gallery" },
-      { name: "Citas", href: "/appointment" },
+      { name: 'Inicio', href: '/' },
+      { name: 'Servicios', href: '/services' },
+      { name: 'Galería', href: '/gallery' },
+      { name: 'Citas', href: '/appointment' },
     ],
+  },
+
+  nuxtIcon: {
+    iconifyApiOptions: {
+      publicApiFallback: true,
+    },
   },
 });
